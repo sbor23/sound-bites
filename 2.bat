9d2bd3e9cd552705669d3a246349ac287e5d1256
Stefan Borer <stefan.borer@imu.unibe.ch>
2017-11-20 09:44:49 +0100

    Add execution script, minor correction

---
+++ 2.bat
@@ -0,0 +1,2 @@
+@echo off
+"C:\Program Files (x86)\PsychoPy2\python.exe" "C:\sound-bites\PsychoPy2\sound-bites.py"
--- Analysis/Ranalysis/1.1_cleanup.R
+++ Analysis/Ranalysis/1.1_cleanup.R
@@ -7,15 +7,15 @@ source("R_Packages+OwnFunctions.R")
 
 load("../data/rawdata.Rdata")
 
-# value is numeric not character
-rawdata$value <- as.numeric(rawdata$value)
 
 # remove .wav from stim_id
 rawdata$stim_id <- sub("(.*)\\.wav", "\\1", rawdata$stim_id)
 
 # split into ratings and searches
 ratings <- rawdata %>%
-  filter(event %in% c("valence_rating", "arousal_rating"))
+  filter(event %in% c("valence_rating", "arousal_rating")) %>%
+  mutate(value = as.numeric(value))
+
 searches <- rawdata %>%
   filter(event == "SearchResponse") %>%
   separate(stim_id, c("sound", "visual"), sep = "-", fill = "left")
@@ -29,7 +29,6 @@ searches <- searches %>%
   mutate(salience = ifelse(salience == "L1", "low", "high")) %>%
   mutate(position = ifelse(grepl("cen", position), "central", "peripheral")) %>%
   mutate(orientation = ifelse(grepl("l", orientation), "left", "right"))
-
 
 save(ratings, file = "../data/ratings.Rdata")
 save(searches, file = "../data/searches.Rdata")
